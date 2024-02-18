dfca1a322ed86abb5d8e0b342a3b9b4d72bdb8b4
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-02-18 22:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let student = {
+	name: "john"
+}
+
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+}
+console.log(student.getKeys())
+document.write(student.getKeys())
